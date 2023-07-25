97dda548f875d1cc7309d7a0d997c50bb6f67a55
Pablo Diaz <pablodiaz989@gmail.com>
2023-07-25 22:41:54 +0200

    'campo de búsqueda'

--- resources/views/livewire/show-threads.blade.php
+++ resources/views/livewire/show-threads.blade.php
@@ -28,7 +28,17 @@
 
     <!-- contenido -->
     <div class="w-full">
-        <!-- formulario -->
+        <!-- campo de busqueda -->
+        <form class="mb-4">
+            <input 
+                type="text" 
+                placeholder="// ..." 
+                class="bg-slate-800 border-0 rounded-mb w-1/3 p-3 text-white/60 text-xs"
+                wire:model="search" 
+            >
+        </form>
+
+        <!-- listado de preguntas -->
         @foreach ($threads as $thread)
             <div class="rounded-md bg-gradient-to-r from-slate-800 to-slate-900 hover:to-slate-800 mb-4">
                 <div class="p-4 flex gap-4">
