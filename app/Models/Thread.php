933bd762e04a89489125f728ed88823a1647c495
Pablo Diaz <pablodiaz989@gmail.com>
2023-07-25 22:04:59 +0200

    'diseño del listado de preguntas'

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Thread extends Model
 {
     use HasFactory;
+
+    // un hilo o pregunta pertenece a una categoría
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    // un hilo o pregunta pertenece a un usuario
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // un hilo o pregunta puede tener muchas respuestas
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
