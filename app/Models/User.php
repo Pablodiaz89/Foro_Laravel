67f9527b05ca8b3e2fcf0acfaba14f1ceedfd3fb
Pablo Diaz <pablodiaz989@gmail.com>
2023-07-27 13:35:20 +0200

    'formulario de creación de respuestas'

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,10 @@ class User extends Authenticatable
     {
         return 'https://gravatar.com/avatar/' . md5($this->email) . '?s=50';
     }
+
+    // un usuario puede tener muchas respuestas
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
